31d17da22bd98fd5c99aeb6c64e8519082767db0
coldwar1942 <chakrit.jira@gmail.com>
2023-08-09 15:27:23 +0700

    Hello World

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+
 function App() {
   return (
     <div>
-      Hello World
+      Hello {"World"}
     </div>
   );
 }
