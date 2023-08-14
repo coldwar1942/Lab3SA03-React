0e7e949efec9200a1936cf18a18bfba30e97f6bb
coldwar1942 <chakrit.jira@gmail.com>
2023-08-14 20:28:09 +0700

    Styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import CharacterCard from "./CharacterCard";
+import './App.css'
 
 const word = 'Hello'
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <div>
             {
-                Array.from(word).map((c,i) => <CharacterCard value={c} key={i}/>)
+                Array.from(word).map((c,i) => <CharacterCard value={c} key={i} class={"card"}/>)
             }
     </div>
   );
